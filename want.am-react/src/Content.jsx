c1b974b68a4b03b054b264d7ad26e26355322571
Nare <narehovhannisyan13.ait@gmail.com>
2024-08-22 22:40:09 +0400

    first commit

---
+++ want.am-react/src/Content.jsx
@@ -0,0 +1,144 @@
+import React from 'react';
+import './Content.css';
+
+function Content(){
+    return(
+        <>
+            <div className='gifts'>
+                <div className='girls'>
+                    <img src="https://want.am/images/thumbnails/70/70/detailed/5/68292-1.jpeg.jpg" alt="img" />
+                    <p>Կանանց նվերներ</p>
+                    <p>219 իր</p>
+                    <p>Սկսած 3900 դ․ից </p>
+                </div>
+                <div className='boys'>
+                    <img src="https://want.am/images/thumbnails/70/70/detailed/2/71102-1.jpeg.jpg" alt="img" />
+                    <p>ՏՂԱՄԱՐԴԿԱՆՑ</p>
+                    <p>309 իր</p>
+                    <p>Սկսած 3000 դ․ից </p>
+                </div>
+                <div className='hobby'>
+                    <img src="https://want.am/images/thumbnails/70/70/detailed/6/U0437307_big.jpeg.jpg" alt="img" />
+                    <p>ՀՈԲԲԻ</p>
+                    <p>1721 իր</p>
+                    <p>Սկսած 2500 դ․ից </p>
+                </div>
+                
+            </div>
+
+
+            <p className='p' >ՊԱՀԱՆՋՎԱԾ</p>
+            <div className='rated'>
+                <div className='rateds'>
+                    <img className='rated-img' src="https://want.am/images/thumbnails/400/400/detailed/2/HTB15qwGHFXXXXXqXFXXq6xXFXXXU.jpg.jpg" alt="image" />
+                    <p className='rated-p'>USB ֆլեշ կրիչ կիթառի տեսքով <br />7900դ</p>
+                </div>
+                <div className='rateds'>
+                    <img className='rated-img' src="https://want.am/images/thumbnails/400/400/detailed/7/dino000.jpeg.jpg" alt="image" />
+                    <p className='rated-p'>Սենսորային  լուսամփոփ<br />10900դ</p>
+                </div>
+                <div className='rateds'>
+                    <img className='rated-img' src="https://want.am/images/thumbnails/400/400/detailed/13/700-nw_xvau-na.jpeg.jpg" alt="image" />
+                    <p className='rated-p'>Սթորմգլասս բյուրեղի տեսքեվ <br />11500դ</p>
+                </div>
+                <div className='rateds'>
+                    <img className='rated-img' src="https://want.am/images/thumbnails/400/400/detailed/6/%D6%84%D6%84%D5%A5%D5%BC%D5%BC_8v1u-zj.jpeg.jpg" alt="image" />
+                    <p className='rated-p'>Աշխարհի սկետչ քարտեզ <br />6500դ</p>
+                </div>
+            </div>
+            <p className='p2' >ԲՈԼՈՐ ՆՎԵՐՆԵՐԸ</p>
+            <div className='rated'>
+                <div className='rateds'>
+                    <img className='rated-img' src="https://want.am/images/thumbnails/400/400/detailed/16/lvh-microscope-3s-ng-01.jpeg.jpg" alt="image" />
+                    <p className='rated-p'>Մանրադիտակ Levenhuk<br />35900դ</p>
+                </div>
+                <div className='rateds'>
+                    <img className='rated-img' src="https://want.am/images/thumbnails/400/400/detailed/16/74116_levenhuk-compass-army-ac10_00.jpeg.jpg" alt="image" />
+                    <p className='rated-p'>Բանակային կողմնացույց<br />10900դ</p>
+                </div>
+                <div className='rateds'>
+                    <img className='rated-img' src="https://want.am/images/thumbnails/400/400/detailed/16/79566_levenhuk-labzz-b2-binoculars_04.jpeg.jpg" alt="image" />
+                    <p className='rated-p'>Հեռադիտակ Levenhuk <br />9900դ</p>
+                </div>
+                <div className='rateds'>
+                    <img className='rated-img' src="https://want.am/images/thumbnails/400/400/detailed/16/%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA_%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B0_2024-06-24_%D0%B2_18.jpeg.jpg" alt="image" />
+                    <p className='rated-p'>«Հարրի Փոթերի փետուրը»<br />6500դ</p>
+                </div>
+            </div>
+            <div className='rated'>
+                <div className='rateds'>
+                    <img className='rated-img' src="https://want.am/images/thumbnails/600/600/detailed/16/%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA_%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B0_2024-06-24_%D0%B2_20.jpeg.jpg" alt="image" />
+                    <p className='rated-p'>Սեղանադիր լամպ<br />39100դ</p>
+                </div>
+                <div className='rateds'>
+                    <img className='rated-img' src="https://want.am/images/thumbnails/600/600/detailed/16/DSC3382.jpeg.jpg" alt="image" />
+                    <p className='rated-p'>Մոդել Ugears<br />28000դ</p>
+                </div>
+                <div className='rateds'>
+                    <img className='rated-img' src="https://want.am/images/thumbnails/600/600/detailed/16/KnightBus_5.jpeg.jpg" alt="image" />
+                    <p className='rated-p'>Ավտոբուս» Հարրի Փոթեր<br />30000դ</p>
+                </div>
+                <div className='rateds'>
+                    <img className='rated-img' src="https://want.am/images/thumbnails/400/400/detailed/16/DSC9244.jpeg.jpg" alt="image" />
+                    <p className='rated-p'>«Ռետրո Մեքենա UGR-T»<br />22500դ</p>
+                </div>
+            </div>
+            <div className='rated'>
+                <div className='rateds'>
+                    <img className='rated-img' src="https://want.am/images/thumbnails/400/400/detailed/16/DSC6452.jpeg.jpg" alt="image" />
+                    <p className='rated-p'>Մոդել Ugears «Պիկապ» <br />31000դ</p>
+                </div>
+                <div className='rateds'>
+                    <img className='rated-img' src="https://want.am/images/thumbnails/600/600/detailed/16/combinate-lock-model-2.jpeg.jpg" alt="image" />
+                    <p className='rated-p'>Կողպեք Կոդով» <br />10050դ</p>
+                </div>
+                <div className='rateds'>
+                    <img className='rated-img' src="https://want.am/images/thumbnails/600/600/detailed/16/Ugears-Mars-Buggy-model-DSC4139.jpeg.jpg" alt="image" />
+                    <p className='rated-p'>«Մարսագնաց»<br />7500դ</p>
+                </div>
+                <div className='rateds'>
+                    <img className='rated-img' src="https://want.am/images/thumbnails/600/600/detailed/16/website-nativity.jpeg.jpg" alt="image" />
+                    <p className='rated-p'>«Սուրբ Ծննդյան Տեսարան»<br />8500դ</p>
+                </div>
+            </div>
+            <div className='rated'>
+                <div className='rateds'>
+                    <img className='rated-img' src="https://want.am/images/thumbnails/600/600/detailed/16/Ugears-Wheel-Organizer-for-pens-pencils-Mechanical-model-DSC4325-Title.jpeg.jpg" alt="image" />
+                    <p className='rated-p'>«Շտուրվալ»  <br />6500դ</p>
+                </div>
+                <div className='rateds'>
+                    <img className='rated-img' src="https://want.am/images/thumbnails/600/600/detailed/16/Ugears-Antique-Box-Mechanical-Model_DSC3310.jpeg.jpg" alt="image" />
+                    <p className='rated-p'> «Հնաոճ Զարդատուփ» <br />23000դ</p>
+                </div>
+                <div className='rateds'>
+                    <img className='rated-img' src="https://want.am/images/thumbnails/400/400/detailed/16/DSC7968.jpeg.jpg" alt="image" />
+                    <p className='rated-p'>«Ժամացույցի Աշտարակ»  <br />8500դ</p>
+                </div>
+                <div className='rateds'>
+                    <img className='rated-img' src="https://want.am/images/thumbnails/600/600/detailed/16/Ugears-Carousel-Mechanical-model-DSC7848.jpeg.jpg" alt="image" />
+                    <p className='rated-p'>«Կարուսել»<br />26000դ</p>
+                </div>
+            </div>
+            <div className='rated'>
+                <div className='rateds'>
+                    <img className='rated-img' src="https://want.am/images/thumbnails/400/400/detailed/15/%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA_%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B0_2024-04-02_%D0%B2_22.jpeg.jpg" alt="image" />
+                    <p className='rated-p'>MTG Innistrad Crimson Oath: <br />1900դ</p>
+                </div>
+                <div className='rateds'>
+                    <img className='rated-img' src="https://want.am/images/thumbnails/400/400/detailed/15/UgearsMechanicalModel20MinTimer2.jpeg.jpg" alt="image" />
+                    <p className='rated-p'>«Թայմեր 20 րոպե» <br />21000դ</p>
+                </div>
+                <div className='rateds'>
+                    <img className='rated-img' src="https://want.am/images/thumbnails/400/400/detailed/15/5265.jpeg.jpg" alt="image" />
+                    <p className='rated-p'>Հարրի Փոթեր «Ֆորդ Անգլիա» <br />8500դ</p>
+                </div>
+                <div className='rateds'>
+                    <img className='rated-img' src="https://want.am/images/thumbnails/600/600/detailed/16/DSC6001.jpeg.jpg" alt="image" />
+                    <p className='rated-p'>«Աերո Ժամացույց» <br />26000դ</p>
+                </div>
+            </div>
+        </>
+    )
+    
+}
+export default Content;
